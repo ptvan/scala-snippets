abe3b5836e8101087092e6a2ba8057dbdb87e173
ptvan <nulzilla@gmail.com>
2021-02-27 07:36:04 -0800

    update

--- learning.scala
+++ learning.scala
@@ -1,2 +1,13 @@
-// anonymous functions
-    (x: Int) => x + 1
+object learning {
+  def abs(n: Int): Int =
+    if (n < 0) -n
+    else n
+
+  private def formatAbs(x: Int) = {
+    val msg = "The absolute value of %d is %d."
+    msg.format(x, abs(x))
+  }
+
+  def main(args: Array[String]): Unit =
+    println(formatAbs(-42))
+}
