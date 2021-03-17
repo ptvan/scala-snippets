60d9fbe552191e85c5a884e9e1e89f2264abf277
ptvan <nulzilla@gmail.com>
2021-03-17 15:17:30 -0700

    first function definition

--- scala_for_the_impatient.scala
+++ scala_for_the_impatient.scala
@@ -6,7 +6,6 @@ BigInt(2).pow(2014)
 // importing
 import scala.BigInt._
 import scala.util._
-
 probablePrime(100, Random)
 
 // create random directory name by converting a random BigInt to base36
@@ -20,3 +19,9 @@ myString.take(4)
 myString.drop(2)
 myString.takeRight(4)
 myString.dropRight(4)
+
+/* CHAPTER 2 */
+
+def signum(x: Int):Int = {
+    if (x == 0) 1 else if (x < 0 ) -1 else 0 
+}
