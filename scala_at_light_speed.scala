4e702c9c07592a80ef041b7be213b1b23faeda5a
ptvan <nulzilla@gmail.com>
2021-05-10 07:34:24 -0700

    updates

--- scala_at_light_speed.scala
+++ scala_at_light_speed.scala
@@ -3,7 +3,10 @@ object Playground extends App {
     val secretNumber: Int = 31337
 
     // string operations
-    val composedString = "strings" + " " + "can" + " " + "be" + " " + "concatenated"
+    val composedString = "a" + " " + "concatenated" + " " + "string"
     val interpolatedString = "the secret is" + s"$secretNumber !"
     
+    // if-expressions
+    // if statements are also expressions
+    val ifExpression = if (secretNumber > 30000) 100 else 200
 }
