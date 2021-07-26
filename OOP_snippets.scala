7bf2cfff001433dc7664a89727ff9416159f5182
Phu T. Van <nulzilla@gmail.com>
2021-07-26 13:07:57 -0700

    converted to Ammonite script

--- OOP_snippets.scala
+++ OOP_snippets.scala
@@ -1,15 +1,26 @@
-import Main.Employee
-
-object Main extends App{
-  // bare definitions throw error so we need Main {} for this to compile
+#!/usr/bin/env amm
 
   // typical polymorphism using inheritance of vanilla classes
 class Employee(var id: Int, var name: String) {
 }
 
+  // declaring salary so need `var`
 class SalaryEmployee(id: Int, name: String, var salary: Int)  extends Employee(id, name)  {
 }
 
-val employee1 = new SalaryEmployee(1, "Alice", 10000)
+val employee1 = new SalaryEmployee(1, "Alice", 20000)
 println(employee1.salary)
+
+// better, more modern polymorphism with case classes and composition
+/** 1. case class doesn't need `new` to instantiate
+    2. it's good practice to make case classes `final`
+       (see https://nrinaudo.github.io/scala-best-practices/tricky_behaviours/final_case_classes.html)
+ **/
+class BetterEmployee(id: Int, name: String) {
 }
+
+case class Manager(id: Int, name: String, var employeesManaged: Int) extends BetterEmployee(id, name) {
+}
+  val employee2 = new Manager(2, "Bob", 1)
+
+println(employee2.employeesManaged)
